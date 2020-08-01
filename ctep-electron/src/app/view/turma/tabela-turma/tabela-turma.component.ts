990c8edc7d0de74263b5d8b11ff564f7d8dd43a0
Arthur Bastos Braga Coelho <arthur.coelho@radixeng.com.br>
2020-08-01 14:49:20 -0300

    listar e salvar turma

--- ctep-electron/src/app/view/turma/tabela-turma/tabela-turma.component.ts
+++ ctep-electron/src/app/view/turma/tabela-turma/tabela-turma.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BaseTable, Coluna } from '../../../custom-components/base-table';
 import { Turma } from '../../../../model/turma.model';
 import { TurmaService } from '../../../../services/turma.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalConfirmacaoComponent } from '../../../custom-components/modal-confirmacao/modal-confirmacao.component';
 
 @Component({
     selector: 'tabela-turma',
@@ -10,7 +12,7 @@ import { TurmaService } from '../../../../services/turma.service';
 })
 export class TabelaTurmaComponent extends BaseTable<Turma> implements OnInit {
 
-    constructor(private turmaService: TurmaService) {
+    constructor(public dialog: MatDialog, private turmaService: TurmaService) {
         super();
     }
 
@@ -18,9 +20,35 @@ export class TabelaTurmaComponent extends BaseTable<Turma> implements OnInit {
         this.columns.push({ key: 'codigo', header: 'Código', field: 'codigo' } as Coluna);
         this.columns.push({ key: 'curso', header: 'Curso', field: 'curso.nome' } as Coluna);
         this.columns.push({ key: 'dia', header: 'Dias da Semana', field: 'diasDaSemana' } as Coluna);
+        this.columns.push({ key: 'buttons', bodyTemplateName: 'buttonsTemplate' } as Coluna);
+        this.buscarTurmas();
+    }
+
+    buscarTurmas() {
         this.turmaService.listarTurmasAtivas().subscribe(data => {
             this.list = data.map(x => Object.assign(new Turma(), x));
         });
     }
 
+    excluirTurma(element: Turma) {
+        const dialogRef = this.dialog.open(ModalConfirmacaoComponent, {
+            data: { mensagem: `Deseja realmente excluir a turma "${element.codigo}"?` }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.turmaService.deletar(element.id).subscribe(() => {
+                    this.buscarTurmas();
+                });
+            }
+        });
+    }
+
+    editarTurma(element: Turma) {
+
+    }
+
+    visualizarTurma(element: Turma) {
+
+    }
 }
