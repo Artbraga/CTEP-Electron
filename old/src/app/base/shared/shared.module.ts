caeaaa5f29d5b76b6f51be49d6866da750abefa1
Arthur Braga <ar.bastos@hotmail.com>
2021-02-03 18:19:14 -0200

    reorganização do projeto

--- old/src/app/base/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CustomTemplate } from './customTemplate';
-@NgModule({
-  imports: [CommonModule],
-  exports: [CustomTemplate],
-  declarations: [CustomTemplate]
-})
-export class SharedModule {}
