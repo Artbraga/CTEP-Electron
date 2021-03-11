bb79b1ef99f5308fc4542a07e86ef2b154546cf9
Arthur Bastos Braga Coelho <arthur.coelho@radixeng.com.br>
2021-03-11 19:52:13 -0300

    registro de turma

--- src/model/registro-turma.model.ts
+++ src/model/registro-turma.model.ts
@@ -1,12 +1,17 @@
 import { BaseConverter } from 'src/app/custom-components/base-converter';
 
 export class RegistroTurma {
+    id: number;
     data: Date;
     turmaId: number;
     registro: string;
 
     get dataStr(): string {
-        return BaseConverter.DateToStringOnlyDate(new Date(this.data));
+        return BaseConverter.DateToStringOnlyDate(this.data);
+    }
+
+    ajustarDatas() {
+        this.data = BaseConverter.StringToDate(this.data.toString());
     }
 
 }
