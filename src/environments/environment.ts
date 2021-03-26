5259fa91511154c6e1ae460d396562134f3ac0ef
Arthur Braga <ar.bastos@hotmail.com>
2021-03-26 18:08:26 -0300

    expansão de tabela

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:5000/api/',
   // apiUrl: 'http://cteptst.brazilsouth.cloudapp.azure.com/ctep-api/api/',
-  loginAutomatico: false
+  loginAutomatico: true
 };
 
 /*
