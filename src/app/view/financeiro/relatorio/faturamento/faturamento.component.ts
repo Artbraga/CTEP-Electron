8fd122eecb9d926f7bdf9459f55a10bfa2631b04
Arthur Braga <ar.bastos@hotmail.com>
2024-01-13 08:58:17 -0300

    Método de relatorio

--- src/app/view/financeiro/relatorio/faturamento/faturamento.component.ts
+++ src/app/view/financeiro/relatorio/faturamento/faturamento.component.ts
@@ -1,12 +1,41 @@
 import { Component, OnInit } from "@angular/core";
-import { BaseConverter } from "src/app/custom-components/base-converter";
+import {
+    MomentDateAdapter,
+    MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from "@angular/material-moment-adapter";
+import {
+    DateAdapter,
+    MAT_DATE_FORMATS,
+    MAT_DATE_LOCALE,
+} from "@angular/material/core";
+import { MatDatepicker } from "@angular/material/datepicker";
+import { Moment } from "moment";
 import { RelatorioMensalFilter } from "src/model/filters/relatorio-mensal.filter";
 import { FinanceiroService } from "src/services/financeiro.service";
+
+export const MY_FORMATS = {
+    parse: {
+        dateInput: "MM/YYYY",
+    },
+    display: {
+        dateInput: "MM/YYYY",
+        monthYearLabel: "MMM YYYY",
+        dateA11yLabel: "LL",
+    },
+};
 
 @Component({
     selector: "faturamento",
     templateUrl: "./faturamento.component.html",
     styleUrls: ["./faturamento.component.scss"],
+    providers: [
+        {
+            provide: DateAdapter,
+            useClass: MomentDateAdapter,
+            deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+        },
+        { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    ],
 })
 export class FaturamentoComponent implements OnInit {
     filter: RelatorioMensalFilter;
@@ -15,8 +44,11 @@ export class FaturamentoComponent implements OnInit {
     ngOnInit(): void {
         this.filter = new RelatorioMensalFilter();
         this.filter.dataFim = new Date();
+        this.filter.dataFim.setDate(1);
         this.filter.dataFim.setMonth(new Date().getMonth() + 6);
+        this.filter.dataFim.setDate(this.filter.dataFim.getDate() - 1);
         this.filter.dataInicio = new Date();
+        this.filter.dataInicio.setDate(1);
         this.filter.dataInicio.setMonth(new Date().getMonth() - 6);
 
         this.buscar();
@@ -29,12 +61,12 @@ export class FaturamentoComponent implements OnInit {
         {
             data: 0,
             stack: "1",
-            label: "Pago",
+            label: "Em Aberto",
         },
         {
             data: 0,
             stack: "1",
-            label: "Em Aberto",
+            label: "Pago",
         },
         {
             data: 0,
@@ -77,9 +109,35 @@ export class FaturamentoComponent implements OnInit {
             .relatorioMensal(this.filter)
             .subscribe((data) => {
                 this.lineChartLabels = data.map((x) => x.mesAno);
-                this.dataSet[0].data = data.map((x) => x.valorPago);
-                this.dataSet[1].data = data.map((x) => x.valorAberto);
+                this.dataSet[0].data = data.map((x) => x.valorAberto);
+                this.dataSet[1].data = data.map((x) => x.valorPago);
                 this.dataSet[2].data = data.map((x) => x.valorNegativado);
             });
+    }
+
+    chosenYearHandler(normalizedYear: Moment, data: string) {
+        if (data == "inicio") {
+            this.filter.dataInicio.setFullYear(normalizedYear.year());
+            this.filter.dataInicio = new Date(this.filter.dataInicio);
+        } else if (data == "fim") {
+            this.filter.dataFim.setFullYear(normalizedYear.year());
+            this.filter.dataFim = new Date(this.filter.dataFim);
+        }
+    }
+
+    chosenMonthHandler(
+        normalizedMonth: Moment,
+        datepicker: MatDatepicker<Moment>,
+        data: string
+    ) {
+        if (data == "inicio") {
+            this.filter.dataInicio.setMonth(normalizedMonth.month());
+            this.filter.dataInicio = new Date(this.filter.dataInicio);
+        } else if (data == "fim") {
+            this.filter.dataFim.setMonth(normalizedMonth.month() + 1);
+            this.filter.dataFim = new Date(this.filter.dataFim.setDate(-1));
+        }
+
+        datepicker.close();
     }
 }
